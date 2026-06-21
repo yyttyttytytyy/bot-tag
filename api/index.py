c099bd4406952879cdabbbc92d53efb832d45b3f
yyttyttytytyy <nahilbehilil@gmail.com>
2026-06-21 14:24:14 +0200

    sup

--- api/index.py
+++ api/index.py
@@ -79,7 +79,7 @@ def main():
                 <h1 style="color: #eedd82; font-size: 48px; text-shadow: 2px 2px 4px rgba(0,0,0,0.5);">
                     Wsp Broksie. This is a private backend!
                 </h1>
-                <p style="font-size: 18px;">Christmas Tag Backend Server Running Smoothly!</p>
+                <p style="font-size: 18px;">Bot Tag Backend Server Running Smoothly!</p>
                 <img src="https://aicdn.picsart.com/275c6ae1-73a4-4cee-b3f5-45ccfa4499ae.png" alt="if u see this text it dont work" style="max-width: 500px; border-radius: 20px; box-shadow: 0 4px 20px rgba(0,0,0,0.3); display: block; margin: 30px auto;">
                 <p style="font-size: 14px; opacity: 0.8;">Image loads when the server works!</p>
             </body>
